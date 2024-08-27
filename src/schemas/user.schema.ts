dd3e2f5545074d02ec63ae976d20dd77755abb5d
emersonjesuz <josiemerson2013@gmail.com>
2024-08-27 10:40:46 -0300

    feat: add schema user

---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,17 @@
+// src/user/schemas/user.schema.ts
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class User extends Document {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop()
+  age: number;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
