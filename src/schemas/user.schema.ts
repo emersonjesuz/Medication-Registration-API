2f8a4b82074f98411760d22d3692d945a73e9ec9
emersonjesuz <josiemerson2013@gmail.com>
2024-08-29 13:39:23 -0300

    fix: fixing the scheme with mongo bank

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,12 +1,16 @@
-import { Schema, Document } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
 
-export const UserSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-});
-
-export interface User extends Document {
-  id: string;
+@Schema()
+export class User extends Document {
+  @Prop({ required: true })
   name: string;
+
+  @Prop({ required: true, unique: true })
   email: string;
+
+  @Prop({ required: true })
+  password: string;
 }
+
+export const UserSchema = SchemaFactory.createForClass(User);
