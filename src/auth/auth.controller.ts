5ff3d6075cb2e3154e6e09a0857eb5ac32b255cc
emersonjesuz <josiemerson2013@gmail.com>
2024-08-29 13:42:35 -0300

    feat: add auth controller

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,41 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  NotFoundException,
+  Post,
+} from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { AuthService } from './auth.service';
+import { ZodValidationPipe } from './pipes/zodValidation.pipe';
+import { createUserSchema } from './zodSchemas/createUser.schemas';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  async login(@Body(new ZodValidationPipe(createUserSchema)) body: any) {
+    const { email, password } = body;
+
+    // get user by email
+    const user = await this.authService.login(email);
+
+    // verify if user exists
+    if (!user) {
+      throw new NotFoundException({
+        message: 'user not found',
+      });
+    }
+
+    // verify if password is correct
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      throw new BadRequestException({
+        message: 'invalid email or password',
+      });
+    }
+
+    return user;
+  }
+}
