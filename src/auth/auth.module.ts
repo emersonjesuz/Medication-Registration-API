19652fe3feccc9ffea316a0aae33fe29e94f6490
emersonjesuz <josiemerson2013@gmail.com>
2024-09-02 14:45:23 -0300

    feat: add jwt to module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,17 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from 'src/schemas/user.schema';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './auth.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
-  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
+  providers: [AuthService, JwtStrategy],
+  imports: [
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({ secret: 'secret', signOptions: { expiresIn: '1d' } }),
+  ],
 })
 export class AuthModule {}
